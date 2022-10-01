9eb870d5890c6d9400f4bceb72eb343f3bf4d4ff
Maureen Wainwright <wainwr1@stolaf.edu>
2022-10-01 18:31:47 -0500

    I added the code from Will that loads in the training and testing data sets.

--- maureen/Lasso Regression Trial.R
+++ maureen/Lasso Regression Trial.R
@@ -1,9 +1,27 @@
 library(Matrix)
 library(glmnet)
-
+library(tidyverse)
+library(rlang)
+library(gt)
+library(caret)
+library(nnet)
+library(broom)
 
 # Load in Will's Final Data
 final_data <- read.csv("final_dataset.csv")
+
+# Load in Will's Training/Testing Data
+ag_dataset <- read_csv('final_dataset.csv')
+
+ag_dataset_model <- ag_dataset %>% 
+  filter(is_satoyama2 %in% c('urban', 'okuyama', 'satoyama')) %>% 
+  mutate(is_satoyama2 = fct_relevel(is_satoyama2, 'satoyama'))
+
+set.seed(1)
+training_samples <- ag_dataset_model$is_satoyama2 %>% 
+  createDataPartition(p = 0.8, list = FALSE)
+training_data  <- ag_dataset_model[training_samples, ]
+testing_data <- ag_dataset_model[-training_samples, ]
 
 # Are there NA values present in the data?
 is.na(final_data)
