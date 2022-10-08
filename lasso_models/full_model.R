ee8ffeef93099a6291b821a6b22f5bc8b6b16c9b
Andrew Noecker <noecke2@stolaf.edu>
2022-10-07 21:39:12 -0500

    Lasso models for natural, social, and full - still need to run the actual models but unsure how to use 3 sets of coefficients

---
+++ lasso_models/full_model.R
@@ -0,0 +1,84 @@
+
+# Full Lasso Model -----------------------------------------------------
+
+
+
+
+# Load in libraries --------------------------------------------------------
+
+library(Matrix)
+library(glmnet)
+library(tidyverse)
+library(rlang)
+library(gt)
+library(caret)
+library(nnet)
+library(broom)
+
+
+# Load in Will's Data and make Training/Testing ---------------------------
+
+# Load in Will's Final Data
+final_data <- read.csv("final_dataset.csv")
+
+# Load in Will's Training/Testing Data
+ag_dataset <- read_csv('final_dataset.csv')
+
+ag_dataset_model <- ag_dataset %>% 
+  filter(is_satoyama2 %in% c('urban', 'okuyama', 'satoyama')) %>% 
+  mutate(is_satoyama2 = fct_relevel(is_satoyama2, 'satoyama'))
+
+set.seed(1)
+training_samples <- ag_dataset_model$is_satoyama2 %>% 
+  createDataPartition(p = 0.8, list = FALSE)
+training_data  <- ag_dataset_model[training_samples, ]
+testing_data <- ag_dataset_model[-training_samples]
+
+# Sort out the different columns for social/natural/full
+cols <- colnames(ag_dataset)
+social <- cols[c(2:41, 47:50)]
+natural <- cols[c(42:46)]
+full <- c(social, natural)
+
+
+# Define training data with just natural variables
+full_train <- training_data[c(full, "is_satoyama2")]
+
+
+# Run lasso on natural training data --------------------------------------
+
+# define x, the explanatory variables
+x <- data.matrix(full_train[, !(colnames(full_train) == "is_satoyama2")])
+
+# define y, the response variable (is_satoyama2)
+y <- full_train$is_satoyama2
+
+# Run lasso using cv.glmnet 
+### This does cross validation for us (10 fold by default)
+
+
+# cross validation and lasso
+set.seed(2)
+full_cv_fit_lasso <- cv.glmnet(x, y, alpha = 1, nfolds = 10, family = "multinomial")
+
+plot(full_cv_fit_lasso,main = " ")
+title(main = "Fig. 3: Lasso Regression with 5-fold Cross Validation", line = 3, font.main = 1)
+
+full_cv_fit_lasso$lambda.min
+full_cv_fit_lasso$lambda.1se
+
+coef(full_cv_fit_lasso, s = "lambda.1se")
+coef(full_cv_fit_lasso, s = "lambda.min")
+
+
+
+# Generate plot of coefficients ------------------------------------------------
+
+# Choose a custom range for lambda: 1/100 to 1000 
+lambdas <- 10^seq(-25, 10, length = 100)
+
+# fit lasso with default lambdas
+full_fit_lasso <- glmnet(x, y, alpha = 1, lambda = lambdas, family = "multinomial")
+
+plot(full_fit_lasso, xvar = "lambda", label = TRUE)
+title(main = "Figure 4: Lasso Regression", line = 3, font.main = 1)
